35633d26350596946f55c28cffe88d3b8563618c
unknown <thanhdoan2000pn@gmail.com>
2020-08-02 00:08:49 +0700

    login user

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\auth;
+use App\User;
+use \Firebase\JWT\JWT;
+use phpDocumentor\Reflection\Types\This;
+
+class LoginController extends Controller
+{
+
+    function createKey()
+    {
+        $myKey = 'doantienthanh200520000946613608';
+        return $myKey;
+    }
+    function Login(REQUEST $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $user = Auth::user();
+            $id_user = $user->id;
+            $data = array('id_user' => $id_user);
+            $myKey = $this->createKey();
+            $token = JWT::encode($data, $myKey);
+            $reponData = array('user_id' => $token, 'status' => 200);
+            return  response()->json($reponData, 200);
+        } else {
+            return array('status' => 400);
+        }
+    }
+    function getProfileUser()
+    {
+        $token = request()->header('Authorization');
+        $myKey = $this->createKey();
+        $data = JWT::decode($token, $myKey, array('HS256'));
+        $user = User::find($data->id_user);
+        $reponData = array('user' => $user);
+        return  response()->json($reponData, 200);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// User login
+Route::post('/user/login','Auth\LoginController@Login');
+Route::post('/user/getProfile','Auth\LoginController@getProfileUser');
